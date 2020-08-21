a8f4e867871d780fd181cded790fb0205890c829
Shuvra Sourav Das <sourav.officialmail@gmail.com>
2020-08-22 01:12:16 +0600

    Created database class library and console app for sample data

---
+++ TimeseriesDataApiSolution/SampleDataConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleDataConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TimeseriesDataApiSolution/TimeseriesDatabase/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TimeseriesDatabase
+{
+    public class Class1
+    {
+    }
+}
